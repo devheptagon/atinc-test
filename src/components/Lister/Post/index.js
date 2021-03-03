18b4951c61b2291b60d09ae27a3a5440c0801b4e
devheptagon <dev@heptagonsoft.com>
2021-03-03 12:06:37 +0000

    task 2 and task 3

--- src/components/Lister/Post/index.js
+++ src/components/Lister/Post/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const Post = ({ title, body, author, onDelete }) => (
-	<div className="postView">
-		<h2>{title}</h2>
-		<p>{body}</p>
-		<p>Posted by: {author}</p>
-		<button onClick={onDelete}>Delete</button>
-	</div>
-)
+const Post = ({ id, title, body, author, onDelete }) => {
+  const handleDelete = () => onDelete(id);
+  return (
+    <div className="postView">
+      <h2>{title}</h2>
+      <p>{body}</p>
+      <p>Posted by: {author}</p>
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  );
+};
 
 export default Post;
--- src/components/Lister/index.js
+++ src/components/Lister/index.js
@@ -1,39 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import getPosts from '../../services/posts';
-import Post from './Post'
-import CreatePost from './CreatePost';
+import React, { useEffect, useState } from "react";
+import getPosts from "../../services/posts";
+import Post from "./Post";
+import CreatePost from "./CreatePost";
 
 const Lister = () => {
+  const [loading, setLoading] = useState(true);
+  const [allPosts, setPosts] = useState([]);
 
-	const [loading, setLoading] = useState(true);
-	const [allPosts, setPosts] = useState([]);
+  useEffect(() => {
+    getPosts().then((data) => {
+      setLoading(false);
+      setPosts(data);
+    });
+  }, []);
 
-	useEffect(() => {
-		getPosts().then(data => {
-			setLoading(false);
-			setPosts(data);
-		});
-	}, []);
+  const onDeletePost = (id) => {
+    const posts = allPosts.filter((p) => p.id !== id);
+    posts.sort((a, b) => a.id - b.id);
+    setPosts(posts);
+  };
 
-	return <div>TODO - wire up lister component</div>
+  const onCreatePost = (post) => {
+    const maxId = !allPosts.length ? 0 : Math.max(...allPosts.map((p) => p.id));
+    const newPost = {
+      id: maxId + 1,
+      ...post,
+    };
+    setPosts([...allPosts, newPost]);
+  };
 
-	const onDeletePost = (id) => {
-		// TODO: implement
-	}
-
-	const onCreatePost = post => {
-		// TODO: implement
-	}
-
-	// TODO: implement render method, using Post and CreatePost e.g.
-	//				...
-	// 				<div className="postList">
-	//					...
-	//					<CreatePost />
-	// 				</div>
-	//				...
-
-
+  return loading ? (
+    "Loading..."
+  ) : (
+    <div className="postList">
+      <CreatePost onCreate={onCreatePost} />
+      {!allPosts.length
+        ? "No posts available..."
+        : allPosts.map((post) => (
+            <Post key={post.id} onDelete={onDeletePost} {...post} />
+          ))}
+    </div>
+  );
 };
 
 export default Lister;
